b9a1ac7e0d4af1ee2f0700572ef82174dfb89189
Ricardo Pereira - FBS <ricardo.pereira@fbsconstrutora.com.br>
2025-12-15 12:02:13 -0300

    exercicios parte 2

---
+++ Exercicios2.py
@@ -0,0 +1,27 @@
+# 1. Avalie se o número digitado pelo usuário é par ou ímpar. Se for par, a saída deve mostrar True, se ímpar, deverá mostrar False.
+
+num1 = int(input('Informe o número desejado: '))
+if num1 % 2 == 0:
+    print(True)
+else:
+    print(False)
+
+
+# # 2. Verifique se o menor preço dessa lista é menor que R$20,00
+# # preços: R$100.20, R$34.90, R$31.50 e R$18.95
+
+menor_preco = min(100.20, 34.90, 31.50, 18.95)
+print(menor_preco)
+
+if menor_preco >= 20.00:
+    print(True)
+else:
+    print(False)
+
+# # 3. Faça um programa que conevrta a temperatura em graus Fahrenheit fornecia pelo usuário em graus Celsius.
+# # celsius = (5/9)*(fahrenheit - 32)
+
+fahrenheit = float(input('Informe a temperatura em Fahrenheit: '))
+
+celsius = (5/9) * (fahrenheit - 32)
+print(f'{celsius} °C')
--- FuncoesBuiltIn.py
+++ FuncoesBuiltIn.py
@@ -9,16 +9,16 @@ print(min(6, 2, 7, 3, 1, 7))   # Resultado: 1
 
 # Max
 # Retorna o valor máximo de uma sequência
-print(max(6, 2, 7, 3, 1)) # Resultado: 7
+print(max(6, 2, 7, 3, 1))  # Resultado: 7
 
 # Sum
 # Faz a soma dos valores de uma sequência
-somaTotal = sum([1,2,3,4])
+somaTotal = sum([1, 2, 3, 4])
 print(somaTotal)
 
 # Pow
 # Faz o cálculo de exponenciação
-print(pow(2,3))
+print(pow(2, 3))
 
 # Print
 # Mostra o valor de saída na tela
@@ -26,7 +26,7 @@ print("Olá mundo")
 
 # Input
 # Lê o valor digitado no terminal pelo usuário
-nome=input('Qual é o seu nome? ')
+nome = input('Qual é o seu nome? ')
 print(nome)
 
 # Help
